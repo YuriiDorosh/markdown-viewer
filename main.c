1bdb3c1227449d9c3b34838b0f5e1e98fdd87d04
yurii <yuriydorosh2005@gmail.com>
2023-08-01 19:06:55 +0300

    UPDATE: import gtk library and basic main function(title and logic for open .md file button

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <gtk/gtk.h>
+
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Markdown Viewer");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    GtkWidget *text_view = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(scroll), text_view);
+    gtk_box_pack_start(GTK_BOX(box), scroll, TRUE, TRUE, 0);
+
+    GtkWidget *button = gtk_button_new_with_label("Open .md File");
+    gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
