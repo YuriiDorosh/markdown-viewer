82c8d3d28714648836fc8c2bb9a6cb0effcffac4
yurii <yuriydorosh2005@gmail.com>
2023-08-01 19:11:47 +0300

    UPDATE: Open .md File functionality

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <gtk/gtk.h>
 
 
+static void show_markdown(GtkButton *button, gpointer user_data) {
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(gtk_file_chooser_dialog_new("Open File", GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(button))), GTK_FILE_CHOOSER_ACTION_OPEN, "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL));
+    GtkFileChooserAction action = gtk_file_chooser_get_action(chooser);
+    const gchar *filename;
+
+    if (gtk_dialog_run(GTK_DIALOG(chooser)) == GTK_RESPONSE_ACCEPT) {
+        filename = gtk_file_chooser_get_filename(chooser);
+    } else {
+        gtk_widget_destroy(GTK_WIDGET(chooser));
+        return;
+    }
+
+    gtk_widget_destroy(GTK_WIDGET(chooser));
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        g_print("Error opening file\n");
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *file_content = (char *)malloc(file_size + 1);
+    if (file_content == NULL) {
+        fclose(file);
+        g_print("Memory allocation error\n");
+        return;
+    }
+
+    fread(file_content, 1, file_size, file);
+    fclose(file);
+    file_content[file_size] = '\0';
+
+    char *html = cmark_markdown_to_html(file_content, file_size, 0);
+
+    gtk_text_buffer_set_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view)), html, -1);
+    g_free(html);
+    g_free(file_content);
+}
+
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -21,6 +63,7 @@ int main(int argc, char *argv[]) {
 
     GtkWidget *button = gtk_button_new_with_label("Open .md File");
     gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+    g_signal_connect(button, "clicked", G_CALLBACK(show_markdown), NULL);
 
     gtk_widget_show_all(window);
     gtk_main();
